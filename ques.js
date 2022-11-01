662efba70da2da0821e5f8dd45fca2177ec29cd5
Pankaj Singh Sirari <pankajsirari222@gmail.com>
2022-11-01 23:36:08 +0530

    minor changes

---
+++ ques.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Inside which HTML element do we put the JavaScript?",
+  answer: "script",
+  options: [
+    "javascript",
+    "js",
+    "src",
+    "script"
+  ]
+},
+  {
+  numb: 2,
+  question: "How many ways are there with which we can declare a variable in javascript?",
+  answer: "Three",
+  options: [
+    "Only one",
+    "Three",
+    "Infinitely many",
+    "None of the above"
+  ]
+},
+  {
+  numb: 3,
+  question: "What will be the output of the following code? <br><br> \< script \>  <br> document.write( typeof( '1' + 2)); <br> \< /script \>",
+  answer: "'string'",
+  options: [
+    "'boolean'",
+    "'string'",
+    "'number'",
+    "None of the Above"
+  ]
+},
+  {
+  numb: 4,
+  question: 'What will be the output of the following code snippet let str="Hello World" console.log(str.charAt(7))',
+  answer: "o",
+  options: [
+    "W",
+    "r",
+    "o",
+    "H"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which of the following method can also be used to create objects in javascript ",
+  answer: "Both a and b",
+  options: [
+    "new keyword",
+    "object.create()",
+    "create.object()",
+    "Both a and b"
+  ]
+}
+];
